d895093e569a43652eca2fa3169c64e3767100fa
Kaming <lokaming86@gmail.com>
2025-11-18 13:59:04 +0700

    Fixing minor bugs

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.future import select
+from sqlalchemy import func
 from contextlib import asynccontextmanager
 
 from extensions import init_db, get_async_session, engine
@@ -11,13 +12,12 @@ from controllers.game_controller import GameController
 
 game_controller = GameController(get_async_session)
 
-# --- Lifespan Normal ---
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("-----------------------------------------")
     print("Lifespan: Memulai aplikasi...")
     await init_db()
-    
+
     print("Lifespan: Startup selesai.")
     print("-----------------------------------------")
     yield
@@ -27,7 +27,6 @@ async def lifespan(app: FastAPI):
     print("Lifespan: Shutdown selesai.")
     print("-----------------------------------------")
 
-# --- FastAPI ---
 app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory="templates")
 
@@ -36,16 +35,28 @@ async def get_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/leaderboard")
-async def get_leaderboard(session: AsyncSession = Depends(get_async_session)):
+async def get_leaderboard():
     try:
-        result = await session.execute(
-            select(Score).order_by(Score.wpm.desc()).limit(10)
-        )
-        scores = result.scalars().all()
-        return [{"username": score.username, "wpm": score.wpm} for score in scores]
+        async with get_async_session() as session:
+            max_wpm_alias = func.max(Score.wpm).label("max_wpm")
+
+            query = (
+                select(
+                    Score.username,
+                    max_wpm_alias
+                )
+                .group_by(Score.username)
+                .order_by(max_wpm_alias.desc())
+                .limit(10)
+            )
+            result = await session.execute(query)
+            scores = result.all()
+
+            return [{"username": row.username, "wpm": row.max_wpm} for row in scores]
+    
     except Exception as e:
         print(f"Error di /leaderboard: {e}")
-        return {"error": "Gagal mengambil leaderboard."}
+        return {"error": "Gagal mengambil leaderboard."}, 500
 
 @app.websocket("/ws/play/{username}")
 async def websocket_play(websocket: WebSocket, username: str):
